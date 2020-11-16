609b9616a7bda269fb9f07b285bcf037ee7c886c
Ritik Goyal <ragoyal2@wisc.edu>
2020-11-15 20:28:43 -0600

    Express route

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,40 @@
+require('dotenv').config();
 import express from 'express';
+const bodyParser = require('body-parser');
+import cors from 'cors';
+import axios from 'axios';
+
 const app = express();
+// Enabling CORS for development
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const PORT = 8000;
+const subdomain = process.env.SUBDOMAIN || '';
+const username = process.env.USERNAME || '';
+const password = process.env.PASSWORD || '';
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+const ticketsURL = `https://${subdomain}.zendesk.com/api/v2/tickets.json`;
+
+app.post('/tickets', async (req, res) => {
+  let link = req.body ? req.body.link : '';
+  let url = link && link !== '' ? link : ticketsURL;
+
+  try {
+    const data = await axios
+      .get(url, {
+        auth: {
+          username,
+          password,
+        },
+      })
+      .then((response) => response.data);
+    res.json(data);
+  } catch (err) {
+    res.status(err.response.status).send(err.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
